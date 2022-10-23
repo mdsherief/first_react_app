c72a873240750aa7573640b1dd4771cac5fc369d
Mohammed Sherief Ibunsali <mdsheriefibunsali@gmail.com>
2022-10-23 13:53:56 +0200

    changes to index.js

--- src/index.js
+++ src/index.js
@@ -1,14 +1,81 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
+// import App from './App';
 import reportWebVitals from './reportWebVitals';
+
+// const root = ReactDOM.createRoot(document.getElementById('root'));
+// root.render(
+//   // <React.StrictMode>
+//   //   <App />
+//   // </React.StrictMode>
+
+//   <React.StrictMode>
+//     <User />
+//   </React.StrictMode>
+// );
+
+
+
+// Customize
+function formatDate(date) {
+  return date.toLocaleDateString();
+}
+
+function Avatar(props) {
+  return (
+    <img className="Avatar"
+         src={props.user.avatarUrl}
+         alt={props.user.name} />
+  );
+}
+
+function UserInfo(props) {
+  return (
+    <div className="UserInfo">
+      <Avatar user={props.user} />
+      <div className="UserInfo-name">
+        {props.user.name}
+      </div>
+    </div>
+  );
+}
+
+function Comment(props) {
+  return (
+    <div className="Comment">
+      <UserInfo user={props.author} />
+      <div className="Comment-text">
+        {props.text}
+      </div>
+      <div className="Comment-date">
+        {formatDate(props.date)}
+      </div>
+      <b><div className="Comment-city">
+        {props.city}
+      </div></b>
+    </div>
+  );
+}
+
+const comment = {
+  date: new Date(),
+  text: 'I hope you enjoy learning React!',
+  author: {
+    name: 'Hello Kitty',
+    avatarUrl: 'http://placekitten.com/g/64/64'
+  },
+  city: "Berlin"
+};
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <Comment
+    date={comment.date}
+    text={comment.text}
+    author={comment.author} 
+    city={comment.city} />
 );
 
 // If you want to start measuring performance in your app, pass a function
